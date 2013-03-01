a4b5ebb8f47c4815b1a41f57d2d18b764f0912c0
Brent Montague <bigbam505@gmail.com>
2013-02-28 22:31:47 -0500

    Add rails Application to the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TagCloud::Application.config.secret_token = '16fedc6045f410c10ec8d86a5ab31df37e5c64ac49540b98f0bb55a68b7ecb4c0039d22f096d06ac09dca1006811d41a7526a2c7800aacc39346b4f14955ae6e'
