d7ed6b646e72e8bfc3a47c1f5d6d90e57ba9d9d1
Brent Montague <bigbam505@gmail.com>
2013-03-01 20:34:44 -0500

    Add Rspec to the project and add a simple true test
    Closes #10

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+# Load Capybara
+require 'capybara/rspec'
+require 'capybara/rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.mock_with :mocha
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+end
