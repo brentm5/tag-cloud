0dffc0c44b57bf3cb99ccea1c8ac51529a015052
Brent Montague <bigbam505@gmail.com>
2013-03-06 20:11:43 -0500

    Add Better errors for development
    Closes #24

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,9 @@ TagCloud::Application.configure do
   $stdout.sync = true
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Allow IP for Better Errors
+  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
